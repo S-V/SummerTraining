279679c15f90b44d1b1a61692157cf6b1f2a390d
CodeGen <myspace@inbox.ru>
2015-07-10 15:02:59 +0400

    ignore .Build folder

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-genie.exe --platform=x64 --file="_Make/_build.lua" --with-sdl --with-tools vs2008
+REM genie.exe --platform=x64 --file="_Make/_build.lua" --with-sdl --with-tools vs2008
+genie.exe --platform=x64 --file="_Make/_build.lua" --with-tools vs2008
 pause
